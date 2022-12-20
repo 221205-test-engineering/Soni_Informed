5eacb0ea3ef1c4624e1c18a2d00ccd6161b01b21
Soni Mohandas <sonireetha@gmail.com>
2022-12-20 01:22:25 -0500

    File edited

--- src/main/java/soni/informed/Level3.java
+++ src/main/java/soni/informed/Level3.java
@@ -1,6 +1,5 @@
 package soni.informed;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.ElementNotInteractableException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -8,9 +7,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
-import java.util.List;
 
 public class Level3 {
     public WebDriver driver;
